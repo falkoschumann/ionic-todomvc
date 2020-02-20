3be584fb3e91c935cc9658959d48652c50a7e986
Falko Schumann <falko.schumann@muspellheim.de>
2020-02-20 21:44:32 +0100

    UI-Mock erstellt

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCheckbox, IonContent, IonFooter, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -7,18 +7,45 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonToolbar color="primary">
+        <IonTitle>todos</IonTitle>
+      </IonToolbar>
+      <IonToolbar>
+        <IonSegment value="all">
+          <IonSegmentButton value="all">
+            <IonLabel>All</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="active">
+            <IonLabel>Active</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="completed">
+            <IonLabel>Completed</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+      </IonToolbar>
+      <IonItem lines="none">
+        <IonCheckbox slot="start"></IonCheckbox>
+        <IonInput placeholder="What needs to be done?"></IonInput>
+      </IonItem>
+    </IonHeader>
+    <IonContent>
+      <IonList>
+        <IonItem>
+          <IonCheckbox slot="start" checked></IonCheckbox>
+          <IonLabel style={{'text-decoration': 'line-through'}}> Taste JavaScript</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonCheckbox slot="start"></IonCheckbox>
+          <IonLabel>Buy a unicorn</IonLabel>
+        </IonItem>
+      </IonList>
+    </IonContent>
+    <IonFooter class="ion-no-border">
+      <IonToolbar>
+        <IonLabel slot="start">1 item left</IonLabel>
+        <IonButton slot="end" fill="clear">Clear completed</IonButton>
+      </IonToolbar>
+    </IonFooter>
     </IonPage>
   );
 };
